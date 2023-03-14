c4f4a5da5988a1257d6180d7ae39f5096ecaaacd
Valloche <127886193+Valloche@users.noreply.github.com>
2023-03-14 19:17:35 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,16 @@ import SEO from '../components/SEO';
 export default function Index({ posts, globalData }) {
   return (
     <Layout>
-      <SEO title={globalData.name} description={globalData.blogTitle} />
-      <Header name={globalData.name} />
-      <main className="w-full">
+      <main>
        <h1 style="text-align: center;">Bienvenue sur le site de Sacha Yoga</h1>
 <p>&nbsp;</p>
 <p><a href="https://sachayogablog.fr/">Sachayogablog</a> est un blog de yoga qui propose une vari&eacute;t&eacute; d'articles int&eacute;ressants sur le yoga et ses bienfaits pour le corps et l'esprit. Le blog aborde divers sujets tels que les<a href="https://sachayogablog.fr/quel-yoga-choisir/"> diff&eacute;rentes formes de yoga</a>, les postures de yoga pour diff&eacute;rents besoins et niveaux, <a href="https://sachayogablog.fr/yoga/respiration/">les techniques de respiration</a>, la <a href="https://sachayogablog.fr/comment-debuter-la-meditation/">m&eacute;ditation</a> et la<a href="https://sachayogablog.fr/que-sont-les-8-piliers-du-yoga/"> philosophie du yoga.</a></p>
 <p>Les articles sont bien &eacute;crits et facilement compr&eacute;hensibles, ce qui les rend accessibles &agrave; tous, m&ecirc;me aux d&eacute;butants en yoga. Le blog propose &eacute;galement des conseils pratiques pour am&eacute;liorer sa pratique de yoga et une section de questions-r&eacute;ponses o&ugrave; les lecteurs peuvent poser des questions et obtenir des r&eacute;ponses de professionnels de yoga.</p>
 <p>Si vous &ecirc;tes int&eacute;ress&eacute; par le yoga et que vous souhaitez en savoir plus sur ses bienfaits, Sachayogablog est un excellent point de d&eacute;part. N'h&eacute;sitez pas &agrave; visiter leur site web pour d&eacute;couvrir leurs articles et leurs sessions de yoga en ligne.</p>
-export function getStaticProps() {
+
+  </main>
+export function getStaticProps() 
+{
   const posts = getPosts();
   const globalData = getGlobalData();
 
