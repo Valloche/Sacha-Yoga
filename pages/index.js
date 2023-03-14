ae806f36fd42126d51ae4dc1ab00062e891f92b7
Valloche <127886193+Valloche@users.noreply.github.com>
2023-03-14 19:15:35 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,51 +14,11 @@ export default function Index({ posts, globalData }) {
       <SEO title={globalData.name} description={globalData.blogTitle} />
       <Header name={globalData.name} />
       <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
-          {globalData.blogTitle}
-        </h1>
-        <ul className="w-full">
-          {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            >
-              <Link
-                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-                href={`/posts/[slug]`}
-              >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.data.date && (
-                    <p className="uppercase mb-3 font-bold opacity-60">
-                      {post.data.date}
-                    </p>
-                  )}
-                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                  {post.data.description && (
-                    <p className="mt-3 text-lg opacity-60">
-                      {post.data.description}
-                    </p>
-                  )}
-                  <ArrowIcon className="mt-4" />
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
-      <Footer copyrightText={globalData.footerText} />
-      <GradientBackground
-        variant="large"
-        className="fixed top-20 opacity-40 dark:opacity-60"
-      />
-      <GradientBackground
-        variant="small"
-        className="absolute bottom-0 opacity-20 dark:opacity-10"
-      />
-    </Layout>
-  );
-}
-
+       <h1 style="text-align: center;">Bienvenue sur le site de Sacha Yoga</h1>
+<p>&nbsp;</p>
+<p><a href="https://sachayogablog.fr/">Sachayogablog</a> est un blog de yoga qui propose une vari&eacute;t&eacute; d'articles int&eacute;ressants sur le yoga et ses bienfaits pour le corps et l'esprit. Le blog aborde divers sujets tels que les<a href="https://sachayogablog.fr/quel-yoga-choisir/"> diff&eacute;rentes formes de yoga</a>, les postures de yoga pour diff&eacute;rents besoins et niveaux, <a href="https://sachayogablog.fr/yoga/respiration/">les techniques de respiration</a>, la <a href="https://sachayogablog.fr/comment-debuter-la-meditation/">m&eacute;ditation</a> et la<a href="https://sachayogablog.fr/que-sont-les-8-piliers-du-yoga/"> philosophie du yoga.</a></p>
+<p>Les articles sont bien &eacute;crits et facilement compr&eacute;hensibles, ce qui les rend accessibles &agrave; tous, m&ecirc;me aux d&eacute;butants en yoga. Le blog propose &eacute;galement des conseils pratiques pour am&eacute;liorer sa pratique de yoga et une section de questions-r&eacute;ponses o&ugrave; les lecteurs peuvent poser des questions et obtenir des r&eacute;ponses de professionnels de yoga.</p>
+<p>Si vous &ecirc;tes int&eacute;ress&eacute; par le yoga et que vous souhaitez en savoir plus sur ses bienfaits, Sachayogablog est un excellent point de d&eacute;part. N'h&eacute;sitez pas &agrave; visiter leur site web pour d&eacute;couvrir leurs articles et leurs sessions de yoga en ligne.</p>
 export function getStaticProps() {
   const posts = getPosts();
   const globalData = getGlobalData();
